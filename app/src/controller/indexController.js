a1cf3233bf6ec952a40bd7de73fd7bbfa6a28d94
James Sproul <jim.sproul@dealertrack.com>
2015-10-09 10:46:51 -0500

    views

--- app/src/controller/indexController.js
+++ app/src/controller/indexController.js
@@ -20,4 +20,14 @@ router.get('/current', function (req, res) {
     logger.debug('index [%s]', req.url);
     res.status(200).render('current', {current: {msg: 'Hello World!'}});
 });
+
+router.get('/topic', function (req, res) {
+    logger.debug('index [%s]', req.url);
+    res.status(200).render('topic', {topic: {msg: 'Hello World!'}});
+});
+
+router.get('/single_topic', function (req, res) {
+    logger.debug('index [%s]', req.url);
+    res.status(200).render('single_topic', {topic: {msg: 'Hello World!'}});
+});
 module.exports = router;
